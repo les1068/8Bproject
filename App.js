427fc6ae22321954333623c599455c35579177c4
dragonone2 <96326628+dragonone2@users.noreply.github.com>
2022-11-23 21:08:03 +0900

    문제만들기

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Question1_1_1 from './Screens/Question1_1_1'
 import Question1_1_2 from './Screens/Question1_1_2'
 import Question1_2_1 from './Screens/Question1_2_1'
 import Question1_3_1 from './Screens/Question1_3_1'
+
 import MainQuestion2 from './Screens/MainQuestion2';
 import SelectStrategy2 from './Screens/SelectStrategy2';
 import Question2_1_1 from './Screens/Question2_1_1'
@@ -13,6 +14,7 @@ import Question2_1_2 from './Screens/Question2_1_2'
 import Question2_2_1 from './Screens/Question2_2_1'
 import Question2_2_2 from './Screens/Question2_2_2'
 import Question2_3_1 from './Screens/Question2_3_1'
+
 import MainQuestion3 from './Screens/MainQuestion3';
 import SelectStrategy3 from './Screens/SelectStrategy3';
 import Question3_1_1 from './Screens/Question3_1_1'
@@ -22,6 +24,7 @@ import Question3_2_2 from './Screens/Question3_2_2'
 import Question3_3_1 from './Screens/Question3_3_1'
 import Question3_3_2 from './Screens/Question3_3_2'
 import Question3_3_3 from './Screens/Question3_3_3'
+
 import MainQuestion4 from './Screens/MainQuestion4';
 import SelectStrategy4 from './Screens/SelectStrategy4';
 import Question4_1_1 from './Screens/Question4_1_1'
@@ -37,6 +40,50 @@ import Question4_3_2 from './Screens/Question4_3_2'
 import Question4_3_3 from './Screens/Question4_3_3'
 import Question4_3_4 from './Screens/Question4_3_4'
 
+import MainQuestion5 from './Screens/MainQuestion5';
+import SelectStrategy5 from './Screens/SelectStrategy5';
+import Question5_1_1 from './Screens/Question5_1_1'
+import Question5_1_2 from './Screens/Question5_1_2'
+import Question5_2_1 from './Screens/Question5_2_1'
+import Question5_2_2 from './Screens/Question5_2_2'
+import Question5_3_1 from './Screens/Question5_3_1'
+import Question5_3_2 from './Screens/Question5_3_2'
+
+import MainQuestion6 from './Screens/MainQuestion6';
+import SelectStrategy6 from './Screens/SelectStrategy6';
+import Question6_1_1 from './Screens/Question6_1_1'
+import Question6_1_2 from './Screens/Question6_1_2'
+import Question6_2_1 from './Screens/Question6_2_1'
+import Question6_2_2 from './Screens/Question6_2_2'
+import Question6_2_3 from './Screens/Question6_2_3'
+import Question6_2_4 from './Screens/Question6_2_4'
+import Question6_3_1 from './Screens/Question6_3_1'
+import Question6_3_2 from './Screens/Question6_3_2'
+
+import MainQuestion7 from './Screens/MainQuestion7';
+import SelectStrategy7 from './Screens/SelectStrategy7';
+import Question7_1_1 from './Screens/Question7_1_1'
+import Question7_1_2 from './Screens/Question7_1_2'
+import Question7_1_3 from './Screens/Question7_1_3'
+import Question7_2_1 from './Screens/Question7_2_1'
+import Question7_2_2 from './Screens/Question7_2_2'
+import Question7_2_3 from './Screens/Question7_2_3'
+import Question7_3_1 from './Screens/Question7_3_1'
+import Question7_3_2 from './Screens/Question7_3_2'
+
+import MainQuestion8 from './Screens/MainQuestion8';
+import SelectStrategy8 from './Screens/SelectStrategy8';
+import Question8_1_1 from './Screens/Question8_1_1'
+import Question8_1_2 from './Screens/Question8_1_2'
+import Question8_1_3 from './Screens/Question8_1_4'
+import Question8_1_4 from './Screens/Question8_1_4'
+import Question8_2_1 from './Screens/Question8_2_1'
+import Question8_2_2 from './Screens/Question8_2_2'
+import Question8_2_3 from './Screens/Question8_2_3'
+import Question8_3_1 from './Screens/Question8_3_1'
+import Question8_3_2 from './Screens/Question8_3_2'
+import Question8_3_3 from './Screens/Question8_2_3'
+
 
 
 const Stack = createStackNavigator();
@@ -50,6 +97,7 @@ export default function App() {
         <Stack.Screen name = "Question1_1_2" component={Question1_1_2}/>
         <Stack.Screen name = "Question1_2_1" component={Question1_2_1}/>
         <Stack.Screen name = "Question1_3_1" component={Question1_3_1}/>
+
         <Stack.Screen name = "MainQuestion2" component={MainQuestion2}/>
         <Stack.Screen name = "SelectStrategy2" component={SelectStrategy2}/>
         <Stack.Screen name = "Question2_1_1" component={Question2_1_1}/>
@@ -57,6 +105,7 @@ export default function App() {
         <Stack.Screen name = "Question2_2_1" component={Question2_2_1}/>
         <Stack.Screen name = "Question2_2_2" component={Question2_2_2}/>
         <Stack.Screen name = "Question2_3_1" component={Question2_3_1}/>
+
         <Stack.Screen name = "MainQuestion3" component={MainQuestion3}/>
         <Stack.Screen name = "SelectStrategy3" component={SelectStrategy3}/>
         <Stack.Screen name = "Question3_1_1" component={Question3_1_1}/>
@@ -66,6 +115,7 @@ export default function App() {
         <Stack.Screen name = "Question3_3_1" component={Question3_3_1}/>
         <Stack.Screen name = "Question3_3_2" component={Question3_3_2}/>
         <Stack.Screen name = "Question3_3_3" component={Question3_3_3}/>
+
         <Stack.Screen name = "MainQuestion4" component={MainQuestion4}/>
         <Stack.Screen name = "SelectStrategy4" component={SelectStrategy4}/>
         <Stack.Screen name = "Question4_1_1" component={Question4_1_1}/>
@@ -80,6 +130,53 @@ export default function App() {
         <Stack.Screen name = "Question4_3_2" component={Question4_3_2}/>
         <Stack.Screen name = "Question4_3_3" component={Question4_3_3}/>
         <Stack.Screen name = "Question4_3_4" component={Question4_3_4}/>
+
+        <Stack.Screen name = "MainQuestion5" component={MainQuestion5}/>
+        <Stack.Screen name = "SelectStrategy5" component={SelectStrategy5}/>
+        <Stack.Screen name = "Question5_1_1" component={Question5_1_1}/>
+        <Stack.Screen name = "Question5_1_2" component={Question5_1_2}/>
+        <Stack.Screen name = "Question5_2_1" component={Question5_2_1}/>
+        <Stack.Screen name = "Question5_2_2" component={Question5_2_2}/>
+        <Stack.Screen name = "Question5_3_1" component={Question5_3_1}/>
+        <Stack.Screen name = "Question5_3_2" component={Question5_3_2}/>
+
+        <Stack.Screen name = "MainQuestion6" component={MainQuestion6}/>
+        <Stack.Screen name = "SelectStrategy6" component={SelectStrategy6}/>
+        <Stack.Screen name = "Question6_1_1" component={Question6_1_1}/>
+        <Stack.Screen name = "Question6_1_2" component={Question6_1_2}/>
+        <Stack.Screen name = "Question6_2_1" component={Question6_2_1}/>
+        <Stack.Screen name = "Question6_2_2" component={Question6_2_2}/>
+        <Stack.Screen name = "Question6_2_3" component={Question6_2_3}/>
+        <Stack.Screen name = "Question6_2_4" component={Question6_2_4}/>
+        <Stack.Screen name = "Question6_3_1" component={Question6_3_1}/>
+        <Stack.Screen name = "Question6_3_2" component={Question6_3_2}/>
+
+        <Stack.Screen name = "MainQuestion7" component={MainQuestion7}/>
+        <Stack.Screen name = "SelectStrategy7" component={SelectStrategy7}/>
+        <Stack.Screen name = "Question7_1_1" component={Question7_1_1}/>
+        <Stack.Screen name = "Question7_1_2" component={Question7_1_2}/>
+        <Stack.Screen name = "Question7_1_3" component={Question7_1_3}/>
+        <Stack.Screen name = "Question7_2_1" component={Question7_2_1}/>
+        <Stack.Screen name = "Question7_2_2" component={Question7_2_2}/>
+        <Stack.Screen name = "Question7_2_3" component={Question7_2_3}/>
+        <Stack.Screen name = "Question7_3_1" component={Question7_3_1}/>
+        <Stack.Screen name = "Question7_3_2" component={Question7_3_2}/>
+
+        <Stack.Screen name = "MainQuestion8" component={MainQuestion8}/>
+        <Stack.Screen name = "SelectStrategy8" component={SelectStrategy8}/>
+        <Stack.Screen name = "Question8_1_1" component={Question8_1_1}/>
+        <Stack.Screen name = "Question8_1_2" component={Question8_1_2}/>
+        <Stack.Screen name = "Question8_1_3" component={Question8_1_3}/>
+        <Stack.Screen name = "Question8_1_4" component={Question8_1_4}/>
+        <Stack.Screen name = "Question8_2_1" component={Question8_2_1}/>
+        <Stack.Screen name = "Question8_2_2" component={Question8_2_2}/>
+        <Stack.Screen name = "Question8_2_3" component={Question8_2_3}/>
+        <Stack.Screen name = "Question8_3_1" component={Question8_3_1}/>
+        <Stack.Screen name = "Question8_3_2" component={Question8_3_2}/>
+        <Stack.Screen name = "Question8_3_3" component={Question8_3_3}/>
+
+
+  
        
 
       </Stack.Navigator>
